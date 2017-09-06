13fe6c911a0914a28e5b28bd72ce3843358b2502
Raimmaster <rai.interiano@gmail.com>
2017-09-06 16:11:05 -0600

    Adding EXPR_TYPE to check the expressions

--- CodeGen/ast.cpp
+++ CodeGen/ast.cpp
@@ -15,6 +15,12 @@
         return this->mips_code.code;    \
     }
 
+#define IMPLEMENT_EXPR_GET_TYPE(className, typeName) \
+    EXPR_TYPE className##Expr::getType() \
+    {   \
+        return typeName##_EXPR; \
+    }   
+
 #define IMPLEMENT_TWO_REG_EXPR_GEN(name, op) \
     string name##Expr::generate() \
     {                                   \
@@ -32,7 +38,14 @@
         return this->mips_code.code;    \
     }
 
-RegisterControl* reg_control = new RegisterControl();
+#define IMPLEMENT_EXPR_EVAL(name, op) \
+    int name##Expr::eval(){ \
+        int v1 = expr1->eval(); \
+        int v2 = expr2->eval(); \
+        return v1 op v2;    \
+    }
+
+    RegisterControl* reg_control = new RegisterControl();
 
 string NumberExpr::generate()
 {
@@ -53,6 +66,42 @@ string VarExpr::generate()
     return this->mips_code.code;
 }
 
-IMPLEMENT_THREE_REG_EXPR_GEN(Add, "add");
+string AddExpr::generate() 
+{                
+    EXPR_TYPE type_expr1 = expr1->getType();                  
+    EXPR_TYPE type_expr2 = expr2->getType();
+    
+    if(type_expr1 == NUM_EXPR || type_expr1 == IMM_EXPR)
+    {
+        if(type_expr2 == NUM_EXPR || type_expr2 == IMM_EXPR)
+        {
+            //Must add immmediates
+            this->type = IMM_EXPR;
+            string empty = "";
+            return empty;
+        }
+    }
+    
+    string v1 = expr1->generate();  
+    string v2 = expr2->generate();      
+    reg_control->freePlace(expr1->mips_code.place); 
+    reg_control->freePlace(expr2->mips_code.place); 
+    stringstream code;      
+    code<<v1<<v2;   
+    this->mips_code.place = reg_control->getNext(); 
+    code<<"add $t"<<this->mips_code.place<<", $t"<<expr1->mips_code.place<<", $t"<<expr2->mips_code.place<<endl; 
+    this->mips_code.code = code.str(); 
+    return this->mips_code.code;    
+}
+
+// IMPLEMENT_THREE_REG_EXPR_GEN(Add, "add");
 IMPLEMENT_THREE_REG_EXPR_GEN(Sub, "sub");
 IMPLEMENT_TWO_REG_EXPR_GEN(Mul, "mult");
+
+IMPLEMENT_EXPR_EVAL(Add, +);
+
+// #IMPLEMENT_EXPR_GET_TYPE(Add, ADD);
+// #IMPLEMENT_EXPR_GET_TYPE(Sub, SUB);
+// #IMPLEMENT_EXPR_GET_TYPE(Mul, MULT);
+// #IMPLEMENT_EXPR_GET_TYPE(Var, VAR);
+// #IMPLEMENT_EXPR_GET_TYPE(Num, NUM);
--- CodeGen/ast.h
+++ CodeGen/ast.h
@@ -14,14 +14,22 @@ using namespace std;
 
 #define SIZE_FLAGS 10
 
-#define DEFINE_BINARY_EXPR(name) \
+enum EXPR_TYPE {
+    NUM_EXPR,
+    ADD_EXPR,
+    SUB_EXPR,
+    MULT_EXPR,
+    VAR_EXPR
+    IMM_EXPR;
+}
+
+#define DEFINE_BINARY_EXPR(name, t) \
     class name##Expr : public BinaryExpr { \
     public:                                 \
-        name##Expr(Expr* expr1, Expr* expr2) : BinaryExpr(expr1, expr2) {} \
+        name##Expr(Expr* expr1, Expr* expr2) : BinaryExpr(expr1, expr2) { this->type = t##_EXPR; } \
         name##Expr() : BinaryExpr() {}                                      \
         string generate(); \
     }
-
 
 class Expr {//abstract by def
 protected:
@@ -29,7 +37,10 @@ protected:
     ~Expr() { }
 public:
     Gen mips_code;
+    EXPR_TYPE type;
+    virtual int eval() = 0;
     virtual string generate() = 0;
+    EXPR_TYPE getType() { return type; }
 };
 
 class BinaryExpr : public Expr
@@ -50,9 +61,11 @@ public:
     NumberExpr(int value) : Expr()
     {
         this->value = value;
+        this->type = NUM_EXPR;
     }
     ~NumberExpr() { }
     int value;
+    int eval() { return value; }
     string generate();
 };
 
@@ -63,12 +76,14 @@ public:
     VarExpr(string* index)
     {
         this->index = *index;
+        this->type = VAR_EXPR;
     }
     string index;
+    int eval() { return 0; }
     string generate();
 };
 
-DEFINE_BINARY_EXPR(Add);
-DEFINE_BINARY_EXPR(Sub);
-DEFINE_BINARY_EXPR(Mul);
+DEFINE_BINARY_EXPR(Add, ADD);
+DEFINE_BINARY_EXPR(Sub, SUB);
+DEFINE_BINARY_EXPR(Mul, MULT);
 #endif
